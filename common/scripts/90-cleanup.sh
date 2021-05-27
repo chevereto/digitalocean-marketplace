5b3f1fdaaa70b870b3cc5ff84662d141c9aae18c
Rodolfo Berrios <inbox@rodolfoberrios.com>
2021-05-27 15:07:22 -0400

    wip

--- common/scripts/90-cleanup.sh
+++ common/scripts/90-cleanup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# DigitalOcean Marketplace Image Validation Tool
+# © 2021 DigitalOcean LLC.
+# This code is licensed under Apache 2.0 license (see LICENSE.md for details)
+
+set -o errexit
+
 # Ensure /tmp exists and has the proper permissions before
 # checking for security updates
 # https://github.com/digitalocean/marketplace-partners/issues/94
@@ -8,14 +14,20 @@ if [[ ! -d /tmp ]]; then
 fi
 chmod 1777 /tmp
 
-apt-get -y update
-apt-get -y upgrade
+if [ -n "$(command -v yum)" ]; then
+    yum update -y
+    yum clean all
+elif [ -n "$(command -v apt-get)" ]; then
+    apt-get -y update
+    apt-get -y upgrade
+    apt-get -y autoremove
+    apt-get -y autoclean
+fi
+
 rm -rf /tmp/* /var/tmp/*
 history -c
 cat /dev/null >/root/.bash_history
 unset HISTFILE
-apt-get -y autoremove
-apt-get -y autoclean
 find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
 rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
 rm -rf /var/lib/cloud/instances/*
