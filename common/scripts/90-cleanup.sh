00ace1076788906af1fbeda94006e533fb7ed732
Rodolfo Berrios <inbox@rodolfoberrios.com>
2021-05-27 15:31:19 -0400

    removes /var/log/{kern,ufw}.log

--- common/scripts/90-cleanup.sh
+++ common/scripts/90-cleanup.sh
@@ -29,7 +29,7 @@ history -c
 cat /dev/null >/root/.bash_history
 unset HISTFILE
 find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
-rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-????????
+rm -rf /var/log/*.gz /var/log/*.log /var/log/*.[0-9] /var/log/*-????????
 rm -rf /var/lib/cloud/instances/*
 rm -f /root/.ssh/authorized_keys /etc/ssh/*key*
 touch /etc/ssh/revoked_keys
